83e1df79f53ceb09a8389263e09bc2104a1ead65
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-07-07 21:00:23 +0530

    ongoing file upload

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,101 +1,60 @@
-import React from 'react'
-import styles from './Dashboard.module.css'
-import { Button, notification, Space } from 'antd';
-
+import React, { useState } from 'react';
 
 export default function Dashboard() {
-    const [api, contextHolder] = notification.useNotification();
-    const openNotificationWithIcon = (type) => {
-        api[type]({
-            message: 'File uploaded successfully',
-            description:
-                'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-        });
-    };
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleSubmit = ()=>{
-        openNotificationWithIcon('success');
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const formData = new FormData();
+    formData.append('AccountNumber', "Acc101");
+    formData.append('CustomerName', "James");
+    formData.append('Balance', 1000);
+    formData.append('File', selectedFile);
+    formData.append('Status', "");
+
+    for (var pair of formData.entries()) {
+        console.log(pair[0]+ ', ' + pair[1]); 
     }
-    return (
-        <div className={styles.main}>
-            {contextHolder}
-            <h1 className='p-4'>Dashboard</h1>
-            <form>
-            <div className='row'>
-                <div className='col-4 p-2'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputFileName" className='p-2'>Filename</label>
-                        <input type="text" className="form-control" id="exampleInputFileName" placeholder="Enter file name..." />
-                    </div>
-                </div>
-                <div className='col-4 p-2'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputSourcePath" className='p-2'>Source Path</label>
-                        <input type="text" className="form-control" id="exampleInputSourcePath" placeholder="Enter source path..." />
-                    </div>
-                </div>
-                <div className='col-4 p-2'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputDestinationPath" className='p-2'>Dest Path</label>
-                        <input type="text" className="form-control" id="exampleInputDestinationPath" placeholder="Enter destination path..." />
-                    </div>
-                </div>
-            </div>
-            <hr />
-            <div className='row'>
-                <div className='col-4'>
-                    <select className="custom-select btn" style={{ "margin-top": "45px" }}>
-                        <option selected>file type</option>
-                        <option value="1">.csv</option>
-                        <option value="2">.pdf</option>
-                        <option value="3">.docs</option>
-                    </select>
-                </div>
-                <div className='col-4'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputVendorName" className='p-2'>Vendor Name</label>
-                        <input type="text" className="form-control" id="exampleInputVendorName" placeholder="Enter vendor name..." />
-                    </div>
-                </div>
-                <div className='col-4'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputVendorName" className='p-2'>Vendor Name</label>
-                        <input type="text" className="form-control" id="exampleInputVendorName" placeholder="Enter vendor name..." />
-                    </div>
-                </div>
-            </div>
-            <hr />
-            <div className='row'>
-                <div className='col-4'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputTemplateDownload" className='p-2'>Template Download</label>
-                        <input type="file" className="form-control" id="exampleInputTemplateDownload" />
-                    </div>
-                </div>
-                <div className='col-4'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputTemplateUpload" className='p-2'>Template Upload</label>
-                        <input type="file" className="form-control" id="exampleInputTemplateUpload" />
-                    </div>
-                </div>
-                <div className='col-4'>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputSampleSourceFile" className='p-2'>Sample Source File</label>
-                        <input type="text" className="form-control" id="exampleInputSampleSourceFile" placeholder="Enter vendor name..." />
-                    </div>
-                </div>
-            </div>
-            <hr />
-            <div className='row'>
-                <Space>
-                    <button className='btn btn-primary' onClick={handleSubmit}>Submit</button>
-                    <input type='reset' className='btn btn-primary' />
-                    {/* <Button onClick={() => openNotificationWithIcon('info')}>Info</Button>
-                    <Button onClick={() => openNotificationWithIcon('warning')}>Warning</Button>
-                    <Button onClick={() => openNotificationWithIcon('error')}>Error</Button> */}
-                </Space>
-            </div>
-            </form>
-        </div>
-    )
+
+
+    fetch('https://localhost:7248/api/fileupload/Called', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+        
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // File uploaded successfully
+          console.log('File uploaded successfully');
+        } else {
+          // Error uploading file
+          console.log(response);
+          console.error('Error uploading file');
+        }
+      })
+      .catch((error) => {
+        console.error('Error uploading file:', error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>File Upload</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} />
+
+        <button type="submit">Upload</button>
+      </form>
+    </div>
+  );
 }

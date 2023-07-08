cf03f4ac74c6b4e97654ddb43ca53c30fb1854fc
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-07-08 11:01:43 +0530

    Fix: File uploaded

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,20 +1,25 @@
+import { event } from 'jquery';
 import React, { useState } from 'react';
 
 export default function Dashboard() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFile2, setSelectedFile2] = useState(null);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
-
+  const handleFileChange2 = (event) =>{
+    setSelectedFile2(event.target.files[0]);
+  }
   const handleSubmit = (event) => {
     event.preventDefault();
     
     const formData = new FormData();
-    formData.append('AccountNumber', "Acc101");
-    formData.append('CustomerName', "James");
-    formData.append('Balance', 1000);
+    formData.append('AccountNumber', "Acc106");
+    formData.append('CustomerName', "Updated3");
+    formData.append('Balance', 10000);
     formData.append('File', selectedFile);
+    formData.append('File2', selectedFile2);
     formData.append('Status', "");
 
     for (var pair of formData.entries()) {
@@ -25,20 +30,21 @@ export default function Dashboard() {
     fetch('https://localhost:7248/api/fileupload/Called', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+         //"Content-Type": "multipart/form-data",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
         
       },
-      body: JSON.stringify(formData),
+      body: formData,
     })
       .then((response) => {
         if (response.ok) {
           // File uploaded successfully
+          console.log(response.json());
           console.log('File uploaded successfully');
         } else {
           // Error uploading file
-          console.log(response);
+          console.log(response.json());
           console.error('Error uploading file');
         }
       })
@@ -52,6 +58,7 @@ export default function Dashboard() {
       <h1>File Upload</h1>
       <form onSubmit={handleSubmit}>
         <input type="file" onChange={handleFileChange} />
+        <input type="file" onChange={handleFileChange2} />
 
         <button type="submit">Upload</button>
       </form>
